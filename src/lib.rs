f5398057db1b10883a1299a7c76a1032d1fc2c8b
sarisssa <mikechansas@gmail.com>
2023-04-02 17:37:13 -0700

    feat: implement try_update_many and try_delete_many methods for datastore

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod mongodb;
-use std::borrow::Borrow;
+use std::{borrow::Borrow, collections::HashMap};
 
 use anyhow::Result;
-use bson::{from_document, to_document, Bson, Document};
+use bson::{from_document, to_document, Document};
 use serde::{Deserialize, Serialize};
 
 use crate::mongodb::Atlas;
@@ -28,44 +28,46 @@ impl Datastore {
         })
     }
 
-    pub async fn try_create<T>(&self, table: &str, record: T) -> Result<Bson>
+    pub async fn try_create<T>(&self, table: &str, record: T) -> Result<T>
     where
-        T: Serialize + Borrow<Document>,
+        T: Serialize + Borrow<Document> + Clone,
     {
         let res = self
             .database
-            .try_insert(&self.database, table, record)
+            .try_insert_one(&self.database, table, record.clone())
             .await?;
 
         let new_record_id = res.inserted_id;
         println!("{:#?}", new_record_id);
 
-        Ok(new_record_id)
+        Ok(record)
     }
 
-    pub async fn try_create_many<T>(&self, table: &str, records: Vec<T>) -> Result<Vec<String>>
+    pub async fn try_create_many<T>(&self, table: &str, records: Vec<T>) -> Result<Vec<T>>
     where
-        T: Serialize + Borrow<Document>,
+        T: Serialize + Borrow<Document> + Clone,
     {
-        let res = self
+        let new_records = records.clone();
+
+        let _ = self
             .database
             .try_insert_many(&self.database, table, records)
             .await?;
 
-        let id_arr = res.inserted_ids.values().map(ToString::to_string).collect();
+        // let id_arr = res.inserted_ids.values().map(ToString::to_string).collect();
 
-        print!("{:#?}", id_arr);
+        // print!("{:#?}", id_arr);
 
-        Ok(id_arr)
+        Ok(new_records)
     }
 
-    pub async fn try_read<T>(&self, table: &str, read_key: String, read_value: String) -> Result<T>
+    pub async fn try_read<T>(&self, table: &str, record_id: String) -> Result<T>
     where
         T: for<'de> Deserialize<'de>,
     {
         let res = self
             .database
-            .try_read::<T>(&self.database, table, read_key, read_value)
+            .try_read_one::<T>(&self.database, table, record_id)
             .await
             .unwrap();
         let read_result = from_document::<T>(res).unwrap();
@@ -81,7 +83,7 @@ impl Datastore {
         Ok(res)
     }
 
-    pub async fn try_update<T>(&self, table: &str, update_record: T) -> Result<T>
+    pub async fn try_update_one<T>(&self, table: &str, update_record: T) -> Result<T>
     where
         T: Serialize + MongoStorable,
     {
@@ -96,15 +98,37 @@ impl Datastore {
         Ok(update_record)
     }
 
-    pub async fn try_delete(&self, table: &str, record_id: String) -> Result<Document> {
-        let res = self
+    pub async fn try_update_many(
+        &self,
+        table: &str,
+        update_map: HashMap<&str, Document>,
+    ) -> Result<Vec<Document>> {
+        let response = self
             .database
-            .try_delete(&self.database, table, record_id)
+            .try_update_many(&self.database, table, update_map)
             .await?;
-        Ok(res)
+        Ok(response)
     }
 
-    pub async fn try_find_documents_by_ids(
+    //Double confirm Delete input. ID vs whole record?
+    //Confirm return nothing or bool?
+    pub async fn try_delete(&self, table: &str, record_id: String) -> Result<()> {
+        let res = self
+            .database
+            .try_delete_one(&self.database, table, record_id)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn try_delete_many(&self, table: &str, delete_ids: Vec<String>) -> Result<()> {
+        let res = self
+            .database
+            .try_delete_many(&self.database, table, delete_ids)
+            .await?;
+        Ok(())
+    }
+
+    pub async fn try_read_documents_by_ids(
         &self,
         table: &str,
         ids: Vec<String>,
@@ -124,13 +148,19 @@ mod datastore_tests {
     use super::*;
     use odds_api::{model::Game, test_data::TestData};
 
-    #[derive(Debug, Serialize)]
-    struct User {
-        _id: String,
+    #[derive(Debug, Deserialize, Serialize)]
+    struct Book {
         name: String,
+        author: String,
     }
 
-    impl MongoStorable for User {
+    #[derive(Debug, Deserialize, Serialize)]
+    struct BookRecord {
+        _id: String,
+        data: Book,
+    }
+
+    impl MongoStorable for BookRecord {
         fn _id(&self) -> &String {
             &self._id
         }
@@ -147,12 +177,15 @@ mod datastore_tests {
     async fn test_02_try_create_one() {
         let db_name = "fnchart";
         let data_store = Datastore::try_new(db_name).await.unwrap();
-        let table = "users";
-        let test_record = doc! {
-            "name": "Harry P",
-            "author": "JKR"
+        let table = "books";
+        let new_record = doc! {
+            "_id": "7e9dd50fdb0b2d895dffc78a239997f7",
+            "data": {
+                "name": "Red Wall",
+                "author": "Brian Jacques"
+            }
         };
-        let res = data_store.try_create(table, test_record).await.unwrap();
+        let res = data_store.try_create(table, new_record).await.unwrap();
     }
 
     #[tokio::test]
@@ -177,12 +210,11 @@ mod datastore_tests {
         let db_name = "fnchart";
         let data_store = Datastore::try_new(db_name).await.unwrap();
 
-        let table = "users";
-        let read_key = "_id".to_string();
-        let read_value = "9c950da2cbab6a4e71437182846961d4".to_string();
+        let table = "books";
+        let record_id = "7e9dd50fdb0b2d895dffc78a239997f7".to_string();
 
         let read_result = data_store
-            .try_read::<Game>(table, read_key, read_value)
+            .try_read::<Document>(table, record_id)
             .await
             .unwrap();
 
@@ -200,18 +232,60 @@ mod datastore_tests {
     }
 
     #[tokio::test]
-    async fn test_06_try_update() {
+    async fn test_06_try_update_one() {
+        //Double Check
         let db_name = "fnchart";
         let data_store = Datastore::try_new(db_name).await.unwrap();
 
-        let table = "users";
+        let table = "books";
 
-        let update_user = User {
-            _id: "1".to_string(),
-            name: "Roman".to_string(),
+        let update_record = doc! {
+            "_id": "7e9dd50fdb0b2d895dffc78a239997f7",
+            "data": {
+                "name": "Red Wall",
+                "author": "Brian Jacques"
+            }
         };
 
-        let update_result = data_store.try_update(table, update_user).await.unwrap();
+        let update_record = from_document::<BookRecord>(update_record).unwrap();
+
+        let update_result = data_store
+            .try_update_one(table, update_record)
+            .await
+            .unwrap();
+        println!("{:#?}", update_result);
+    }
+
+    #[tokio::test]
+    async fn test_06_try_update_many() {
+        let db_name = "fnchart";
+        let data_store = Datastore::try_new(db_name).await.unwrap();
+
+        let mut update_map = HashMap::new();
+
+        let table = "books";
+
+        let update_record1 = doc! {
+            "_id": "0aa7ba9d4ef9dfacd6c1d4e545b86e87"
+        ,
+            "data": {
+                "name": "Dagon",
+                "author": "HP Lovecraft"
+            }
+        };
+
+        let update_record2 = doc! {
+            "_id": "7e9dd50fdb0b2d895dffc78a239997f7",
+            "data": {
+                "name": "The Long Patrol",
+                "author": "Brian Jacques"
+            }
+        };
+
+        update_map.insert("0aa7ba9d4ef9dfacd6c1d4e545b86e87", update_record1);
+        update_map.insert("7e9dd50fdb0b2d895dffc78a239997f7", update_record2);
+
+        let update_result = data_store.try_update_many(table, update_map).await.unwrap();
         println!("{:#?}", update_result);
     }
 
@@ -241,7 +315,7 @@ mod datastore_tests {
         let ids = vec!["1".to_string(), "2".to_string(), "3".to_string()];
 
         let update_result = data_store
-            .try_find_documents_by_ids(table, ids)
+            .try_read_documents_by_ids(table, ids)
             .await
             .unwrap();
         println!("{:#?}", update_result);
